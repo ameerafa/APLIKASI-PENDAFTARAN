59fee6b8b6b4f27fa6fa772effbbaf51a40915af
ameerafa <124320755+ameerafa@users.noreply.github.com>
2023-02-03 11:05:12 +0700

    Add files via upload

---
+++ home.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+	<html>
+		<head>
+		<title></title>
+			</head>
+			<body bgcolor="white">
+				<!-- bgcolor untuk menampilkan warna pada background -->
+
+				<h1>Sunmori</h1>
+				
+				<p>Sunmori adalah salah satu istilah yang pasti melekat di kalangan para pecinta sepeda motor.
+				salah satu agenda rutin dari club motor untuk berkumpul dengan anggotanya. 
+				sunmori ini memiliki peserta yang tidak terbatas, mulai dari skala kecil hingga diikuti oleh banyak orang.
+			Istilah ini banyak digunakan oleh komunitas pecinta motor yang ada di seluruh Indonesia dengan peserta dari berbagai macam kalangan.
+		Biasanya kegiatan ini diadakan sebagai agenda rutin dimana sesama anggota suatu club motor akan berkumpul di suatu tempat, lalu melakukan riding.
+	Kegiatan riding dengan jarak yang sudah ditentukan ini menjadi salah satu hal menyenangkan bagi pecinta motor.
+	Apalagi ditambah dengan berbagai kegiatan seperti Games,Doorprize,Bonfire,live music,Barbeque dan camp. 
+	ayo gabung !!
+	 14-15 Mei 2022,MOJOKERTO.BURUAN DAFTAR JANGAN SAMPAI KETINGGALAN !!!!</p>
+
+			
+
+					<a href="login.php">Pendaftaran</a> <br>
+			</body>
+	</html>
